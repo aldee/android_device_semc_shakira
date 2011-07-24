59ed2949e12b50a2c308c4c6fc996a0fd3ec144a
jerpelea <jerpelea@gmail.com>
2011-07-24 06:48:56 +0300

    shakira: updated camera

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),shakiraa)
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),shakira)
 
 # When zero we link against libmmcamera; when 1, we dlopen libmmcamera.
 DLOPEN_LIBMMCAMERA:=1
