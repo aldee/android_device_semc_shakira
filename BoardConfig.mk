0244d4c40e65e7756e9abd2d3d075daabccbd70b
jerpelea <jerpelea@gmail.com>
2011-08-11 18:04:42 +0300

    semc:lights and camera fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,4 +3,6 @@
 TARGET_BOOTLOADER_BOARD_NAME := shakira
 TARGET_SPECIFIC_HEADER_PATH := device/semc/shakira/include
 
+TARGET_OTA_ASSERT_DEVICE := X8i,X8a,shakira
+
 -include device/semc/msm7x27-common/Android.mk
